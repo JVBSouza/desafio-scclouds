2467a8f74790a7986098604b8287d606cdc193f1
JVBSouza <joseboing@gmail.com>
2021-10-04 21:57:13 -0300

    Add main logic file and update readme

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# desafio-scclouds
-Desafio técnico para a vaga de estágio na SC Clouds
+<br />
+<p align="center">
+  <h2 align="center">SC Clouds challenge</h2>
+  <p align="center">
+    Developed by: José Vinícius Boing de Souza.
+  </p>
+</p>
+<br />
+
+## Summary:
+
+This simple project was developed for the SCClouds internship challenge to demonstrate the candidates programming skill. The choosen language was [Python](https://www.python.org/).
+
+## Challenge:
+
+Develeop a linear and recursive function for the following conditions:
+
+1. A function that returns the value of the position N ofs fibonacci order.
+
+- Ex: F(0) = 0; F(1) = 1; F(6) = 8;
+
+2. A function that returns all the primes number in the range from 0 to N.
+
+- Ex: F(10) = [2, 3, 5, 7]
+
+## Installation and execution
+
+1. Clone or download this repository. <br>
+2. Install Python
+3. No extra dependecy is required. To run the code, exercute:
+
+```sh
+python run.py
+```
---
+++ run.py
@@ -0,0 +1,80 @@
+def fibonnaci_linear(number):
+    if type(number) == float:
+        return 'Choose a intenger'
+    if number < 0:
+        return "Choose a natural number"
+    if number == 0:
+        return 0
+    elif number == 1:
+        return 1
+    position_0 = 0
+    position_1 = 1
+    for iteration in range(2, number + 1, 1):
+        position_new = position_0 + position_1
+        position_0 = position_1
+        position_1 = position_new
+        
+    return position_1
+    
+def fibonnaci_recursive(number):
+    if type(number) == float:
+        return 'Choose a intenger'
+    if number < 0:
+        return "Choose a natural number"
+    if number == 0:
+        return 0
+    if number == 1:
+        return 1
+    return fibonnaci_recursive(number-1) + fibonnaci_recursive(number-2)
+
+def prime_linear(number):
+    if type(number) == float:
+        return 'Choose a intenger'
+    if number < 2:
+        return "Choose a number higher than one"
+    prime_list = []
+    for position in range(2, number+1, 1):
+        if position == 2:
+            prime_list.append(2)
+            continue
+        for divider in range(2, position, 1):
+            if position % divider == 0:
+                break
+            if divider * divider > position:
+                prime_list.append(position)
+                break    
+    return prime_list
+    
+def prime_recursive_checker(number, divider = 2):
+    if number % divider == 0 :
+        return False
+    if divider * divider > number:
+        return True
+    
+    return prime_recursive_checker(number, divider + 1)
+
+def prime_recursive(number):
+    if type(number) == float:
+        return 'Choose a intenger'
+    if number < 2:
+        return "Choose a number higher than one"
+    if number == 2:
+        return [2]
+    prime_list = [2]
+    for position in range(3,number+1,1):
+        if prime_recursive_checker(position):
+            prime_list.append(position)
+    
+    return prime_list
+
+def main():
+    print("Fibonnaci position")
+    print("Linear function:", fibonnaci_linear(6))
+    print("Recursive function:", fibonnaci_recursive(6))
+    print("")
+    print("Prime number list")
+    print("Linear function:", prime_linear(10))
+    print("Recursive function:", prime_recursive(10))
+
+if __name__ == "__main__":
+    main()
